1ef24cd745e6464763279260d8c8fcd162290039
Ben Morgan <benmorgantd@gmail.com>
2026-07-17 17:54:26 -0700

    pushed changes to files, adjusted defaults for film sim

--- photo_editor_core.py
+++ photo_editor_core.py
@@ -149,12 +149,12 @@ class PhotoEditor:
                     "do_instagram_compression": True
                 }
             },
+            # 1. Tonality & Color Science
             "rgb_curves" : {
                 "r" : [[0.0, 0.25, 0.50, 0.75, 1.0], [0.0, 0.25, 0.50, 0.75, 1.0]],
                 "g" : [[0.0, 0.25, 0.50, 0.75, 1.0], [0.0, 0.25, 0.50, 0.75, 1.0]],
                 "b" : [[0.0, 0.25, 0.50, 0.75, 1.0], [0.0, 0.25, 0.50, 0.75, 1.0]]
             },
-            # 1. Tonality & Color Science
             "color_matrix": [
                 [1.0, 0.0, 0.0],
                 [0.0, 1.0,  0.0],
@@ -165,20 +165,20 @@ class PhotoEditor:
             "enable_bloom": True,
             "bloom_threshold": 0.70,
             "bloom_radius": 15.0,
-            "bloom_strength": 0.15, 
+            "bloom_strength": 0.20, 
             
             # 3. Selective Halation
             "enable_halation": True,
             "halation_threshold": 0.60,
-            "halation_radius": 8.0,
-            "halation_strength": 0.30,
+            "halation_radius": 12.0,
+            "halation_strength": 0.35,
             "halation_offset_x": 1.5,
-            "halation_offset_y": 0.0,
+            "halation_offset_y": 0.2,
             
             # 4. Smart Grain
             "enable_grain": True,
             "grain_strength": 0.05, 
-            "grain_size": 1.6,      # >1.0 scales grain up for coarse emulsion
+            "grain_size": 0.08,      # >1.0 scales grain up for coarse emulsion
             "grain_chroma": 0.15,   # 0.0 = B&W grain, 1.0 = color grain
             
             # 5. Vignette & Optical Softness
@@ -186,7 +186,7 @@ class PhotoEditor:
             "vignette_strength": 0.35,
             "vignette_radius": 0.75,
             "vignette_softness": 0.45,
-            "corner_blur_radius": 3.0
+            "corner_blur_radius": 1.0
         }
 
     def __init__(self, image_path: str):
